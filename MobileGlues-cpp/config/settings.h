b1cd857b4a9370998244237ae32a3c59cc1cc9c5
Uniaball <2551733616@qq.com>
2026-02-12 15:36:07 +0800

    [Fix] (setting.h): Mysterious git error.

--- MobileGlues-cpp/config/settings.h
+++ MobileGlues-cpp/config/settings.h
@@ -174,4 +174,9 @@ struct global_settings_t {
 
 extern global_settings_t global_settings;
 
-void init_settin
+void init_settings();
+void init_settings_post();
+std::string dump_settings_string(std::string prefix = "");
+void set_multidraw_setting();
+
+#endif // MOBILEGLUES_PLUGIN_SETTINGS_H
