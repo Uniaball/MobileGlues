a60347aaeb23a638c996e51ea03d5b8ecd78292d
Uniaball <2551733616@qq.com>
2026-07-11 09:57:10 +0800

    [Fix] (glsl): Fix the missing

--- MobileGlues-cpp/gl/glsl/glsl_for_es.cpp
+++ MobileGlues-cpp/gl/glsl/glsl_for_es.cpp
@@ -734,6 +734,8 @@ std::vector<unsigned int> glsl_to_spirv(GLenum shader_type, int glsl_version, co
     glslang::TShader shader(shader_language);
     shader.setStrings(shader_src, 1);
 
+    using namespace glslang;
+
     shader.setEnvInput(EShSourceGlsl, shader_language, EShClientVulkan, glsl_version);
     shader.setEnvClient(EShClientOpenGL, EShTargetOpenGL_450);
     shader.setEnvTarget(EShTargetSpv, EShTargetSpv_1_5);
