b198a28a708d6ba1518b7b9cb80daee07cc39798
Uniaball <2551733616@qq.com>
2026-07-12 16:00:33 +0800

    [Feat] (build): Add the cmake configuration for lto_level and extra_optimizations in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,10 @@ android {
 
     defaultConfig {
         minSdk = 21
-
         ndkVersion = "27.3.13750724"
+        ndk {
+            abiFilters.add("arm64-v8a")
+        }
     }
 
     buildTypes {
@@ -28,6 +30,14 @@ android {
         cmake {
             path = file("MobileGlues-cpp/CMakeLists.txt")
             version = "3.22.1"
+            arguments.add("-DUSE_EXTRA_OPTIMIZATIONS=" + (System.getenv("USE_EXTRA_OPTIMIZATIONS") ?: "ON"))
+            val ltoLevel = System.getenv("LTO_LEVEL")?.toIntOrNull() ?: 0
+            val ltoFlag = when (ltoLevel) {
+                2 -> "-flto"
+                1 -> "-flto=thin"
+                else -> ""
+            }
+            arguments.add("-DLTO_FLAG=" + ltoFlag)
         }
     }
 
